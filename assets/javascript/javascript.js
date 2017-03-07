05bc386ed774ed9b9745389fd8e46e9790aaf2f7
Caryn Carter <caryn.carter@northwestern.com>
2017-03-07 13:04:57 -0600

    fixed html screen and pictures

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+var moves = ["American Ballet Theater", "Fouette", "Misty Copeland", "Pirouettes", "Pointe Shoes", "Grand Jete", "Ballet Barre", "Arabesque"];
+
+for (var i = 0; i < moves.length; i++) {
+	$(".buttons").append(moves[i]);
+}
+
+
+
+
+});
