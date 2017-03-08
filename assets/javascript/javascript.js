f12122a17f97b92a20eba9d46bc22634aa3e737e
Caryn Carter <caryn.carter@northwestern.com>
2017-03-08 11:46:14 -0600

    added buttons , query URL

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,62 @@
 $(document).ready(function () {
 
-var moves = ["American Ballet Theater", "Fouette", "Misty Copeland", "Pirouettes", "Pointe Shoes", "Grand Jete", "Ballet Barre", "Arabesque"];
+var moves = ["Svetlana Zakharova", "American Ballet Theater", "Fouette", "Misty Copeland", "Pirouettes", "Pointe Shoes", "Grand Jete", "Ballet Barre", "Arabesque"];
+var buttonValue = ""; 
+var queryURL = ""; 
+displayButtons (); 
+renderGifs(); 
 
-for (var i = 0; i < moves.length; i++) {
-	$(".buttons").append(moves[i]);
+function displayButtons () {
+	for (var i = 0; i < moves.length; i++) {
+		var moveButton = $("<button type='button' class='btn btn-default move-button' value=" + moves[i] + ">" + moves[i] + "</button>");
+		$(".buttons-area").append(moveButton);
+	}
 }
 
+function renderGifs () {
+	$(".move-button").on("click", function() {
+		buttonValue = $(this).attr("value"); 
+		console.log(buttonValue);
+		queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + buttonValue + "&api_key=dc6zaTOxFJmzC&limit=10";
+		console.log(queryUrl);
+		
+		
+		
+	});
+}
+		
 
+			// $.ajax({
+	// 	url: queryURL,
+ //        method: "GET"
+ //     })
+	// 	.done(function(response) {
+ //         var results = response.data;
+ //         console.log(response.data);
+	// });
+//       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+//         person + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+//       $.ajax({
+//           
+//         
+
+//           for (var i = 0; i < results.length; i++) {
+//             var gifDiv = $("<div class='item'>");
+
+//             var rating = results[i].rating;
+
+//             var p = $("<p>").text("Rating: " + rating);
+
+//             var personImage = $("<img>");
+//             personImage.attr("src", results[i].images.fixed_height.url);
+
+//             gifDiv.prepend(p);
+//             gifDiv.prepend(personImage);
+
+//             $("#gifs-appear-here").prepend(gifDiv);
+//           }
+//         });	
 
 
 });
