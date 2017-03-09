95be4ade21b53b802d5033514e57a851a14dd4d3
Caryn Carter <caryn.carter@northwestern.com>
2017-03-09 11:14:50 -0600

    on submit gifs appear too and state of still put in place

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,31 +1,13 @@
 $(document).ready(function () {
 
 var moves = ["Svetlana Zakharova", "American Ballet Theater", "Fouette", "Corps de Ballet", "Grand Jete", "Pas de Deux", "Arabesque", "Paris Opera Ballet"];
-var selectedButtonValue = ""; 
-var queryURL = ""; 
-displayButtons (); 
-getButtonValue(); 
-
-/* function displays buttons for item in the array and trims white space at the end of each array item if any */
-function displayButtons () { 
-	for (var i = 0; i < moves.length; i++) {
-		moves[i]= moves[i].trim(); 
-		var moveButton = $("<button type='button' class='btn btn-default move-button'>" + moves[i] + "</button>");
-		$(".buttons-area").append(moveButton);
-	}
-}
-
-/*function adds new buttons for new dance terms */
-$(".add-move").on("click", function (event) {
-	event.preventDefault(); 
-	var newMove = $("#move").val(); 
-		console.log(newMove);		
-	moves.push(newMove);
-		console.log(moves);
-	$(".buttons-area").html("");
-		displayButtons();
-		getButtonValue(); 
-});
+var selectedButtonValue; 
+var queryURL; 
+var newMove;
+var state; 
+displayButtons(); 
+getButtonValue ();
+addNew(); 
 
 /*function gets button value which is the text of the button and replaces any white space with +. */
 function getButtonValue () {
@@ -37,16 +19,48 @@ function getButtonValue () {
 			selectedButtonValue = selectedButtonValue.replace(/\s/g, "+");
 		}
 		console.log(selectedButtonValue);
-		renderGifs(); 
+		getUrl(selectedButtonValue); 
+		// addNew(); 
 	});
-	
+}	
+
+/*function adds new buttons for new dance terms */
+function addNew () {
+	$(".add-move").on("click", function (event) {
+		event.preventDefault(); 
+		newMove = $("#move").val(); 
+			console.log(newMove);		
+		moves.push(newMove);
+			console.log(moves);
+		$(".buttons-area").empty();
+			displayButtons();
+		$(".giphs").html("<br><br>");
+		if(newMove.indexOf(" ") >= 0){
+			newMove = newMove.replace(/\s/g, "+");
+		}
+		getUrl(newMove); 
+		getButtonValue();
+
+	});
+}
+
+/* function displays buttons for item in the array and trims white space at the end of each array item if any */
+function displayButtons () { 
+	for (var i = 0; i < moves.length; i++) {
+		moves[i]= moves[i].trim(); 
+		var moveButton = $("<button type='button' class='btn btn-default move-button'>" + moves[i] + "</button>");
+		$(".buttons-area").append(moveButton);
+	}
 }
 
 /* goes out to the giphy API and renders the first 10 gifs for the topic of the button */
-function renderGifs () {
-	queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + selectedButtonValue + "&api_key=dc6zaTOxFJmzC&limit=10";
+function getUrl (moveTerm) {
+	queryUrl = "http://api.giphy.com/v1/gifs/search?q=" +  moveTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
 		console.log(queryUrl);
-		
+	renderGifs(); 
+}
+
+function renderGifs () {		
 	$.ajax({
 		url: queryUrl,
         method: "GET"
@@ -57,15 +71,17 @@ function renderGifs () {
 
          for (var i = 0; i < results.length; i++) {
          	var rating = response.data[i].rating.toUpperCase(); 
-         	$(".giphs").append("<figure><img class='gifAnimate' src='" + response.data[i].images.fixed_height.url+ "'>");
+         	$(".giphs").append("<figure><img class='gifAnimate' src='" + response.data[i].images.fixed_height_still.url+ "'>");
          	$(".giphs").append("<figcaption class='caption'>Rating: " + rating + "</figcaption></figure><br>");	
          }
+
+         $(".gifAnimate").on("click", function () {
+         	$(".")
+         });
 
 		});
 		
 }
-
-
 
 
 
