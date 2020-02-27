607d393db23ad26e00f79e9bd27aea3a8ac77cac
Caryn Carter <carter.caryn@gmail.com>
2020-02-27 10:00:43 -0600

    modfied file description

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php include_once("index.html"); ?>
+<?php include_once("index.html"); 
+//This file has to exist for Heroku to recognize the app as a PHP app, 
+//which is the easiest kind of app to run in Heroku.
+
+?>
